2c0000893bb43a4b08c9f155c0032a20fabd9607
0anunew <0anunew@gmail.com>
2025-10-07 15:39:10 +0700

    push

--- app/src/main/java/com/example/roomDB/MainActivity.kt
+++ app/src/main/java/com/example/roomDB/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomDB.adaptors.NoteAdaptor
 import com.example.roomDB.model.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var noteViewModel: NoteViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var notesAdaptor: NoteAdaptor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById(R.id.recycler_view)
+        notesAdaptor = NoteAdaptor()
+        recyclerView.adapter = notesAdaptor
 
         noteViewModel = ViewModelProvider(
             this,
---
+++ app/src/main/java/com/example/roomDB/adaptors/NoteAdaptor.kt
@@ -0,0 +1,41 @@
+package com.example.roomDB.adaptors
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomDB.R
+import com.example.roomDB.entity.Note
+
+class NoteAdaptor() : RecyclerView.Adapter<NoteAdaptor.NoteViewHolder>() {
+    private var notesList = mutableListOf<Note>()
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NoteViewHolder {
+        return NoteViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false))
+    }
+
+    override fun onBindViewHolder(
+        holder: NoteViewHolder,
+        position: Int
+    ) {
+        holder.textTitle.text = notesList[position].title
+        holder.textContent.text = notesList[position].content
+        holder.textPriority.text = notesList[position].priority.toString()
+    }
+
+    override fun getItemCount() = notesList.size
+
+    fun setNotes(notes:MutableList<Note>){
+        notesList = notes
+    }
+    inner class NoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var textTitle: TextView = itemView.findViewById(R.id.title)
+        var textContent: TextView = itemView.findViewById(R.id.content)
+        var textPriority: TextView = itemView.findViewById(R.id.priority)
+
+    }
+}
