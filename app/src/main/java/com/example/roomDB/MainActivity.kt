43f04430fb59ad9ebdee2561e4ea3c0ef578cb2b
0anunew <0anunew@gmail.com>
2025-10-08 10:41:35 +0700

    push

--- app/src/main/java/com/example/roomDB/MainActivity.kt
+++ app/src/main/java/com/example/roomDB/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.roomDB
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roomDB.activities.AddUpdateActivity
 import com.example.roomDB.adaptors.NoteAdaptor
+import com.example.roomDB.entity.Note
 import com.example.roomDB.model.NoteViewModel
+import com.example.roomDB.utils.Constants
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var noteViewModel: NoteViewModel
     private lateinit var recyclerView: RecyclerView
     private lateinit var notesAdaptor: NoteAdaptor
+    private lateinit var addButton: FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        addButton = findViewById(R.id.add_note_button)
         recyclerView = findViewById(R.id.recycler_view)
         notesAdaptor = NoteAdaptor()
         recyclerView.adapter = notesAdaptor
@@ -41,5 +50,20 @@ class MainActivity : AppCompatActivity() {
             notesAdaptor.setNotes(it)
         }
 
+        val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Constants.REQUEST_CODE) {
+                val saveTitle = it.data?.getStringExtra(Constants.EXTRA_TITLE)
+                val saveContent = it.data?.getStringExtra(Constants.EXTRA_DESCRIPTION)
+                val savePriority = it.data?.getIntExtra(Constants.EXTRA_PRIORITY, 1)
+
+                val  saveNote = Note(saveTitle!!, saveContent!!, savePriority!!)
+                noteViewModel.addNote(saveNote)
+            }
+        }
+
+        addButton.setOnClickListener {
+            val intent = Intent(this, AddUpdateActivity::class.java)
+            getResult.launch(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/roomDB/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.roomDB.utils
+
+object Constants {
+    const val REQUEST_CODE = 111
+    const val EXTRA_TITLE = "Title"
+    const val EXTRA_DESCRIPTION = "Description"
+    const val EXTRA_PRIORITY = "Priority"
+}
